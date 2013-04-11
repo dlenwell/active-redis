be41883876491b3a3a53b7a4e332bc625666eac8
Jordan Halterman <jordan.halterman@gmail.com>
2013-04-10 23:48:07 -0700

    Add observables.

---
+++ active_redis/observables/__init__.py
@@ -0,0 +1,23 @@
+from active_redis.core import Observable
+from active_redis.registry import Observable as Registry
+
+@Registry.register
+class List(Observable):
+  """List observable."""
+  type = list
+  watch_methods = [
+    '__setitem__',
+    'append',
+    'extend',
+    'insert',
+    'remove',
+    'pop',
+  ]
+
+@Registry.register
+class Dict(Observable):
+  """Dictionary observable."""
+  type = dict
+  watch_methods = [
+    '__setitem__',
+  ]
